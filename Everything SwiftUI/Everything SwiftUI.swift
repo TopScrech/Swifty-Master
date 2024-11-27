9adf8388521c0a433c5ca28f2e4fc0b6d876967c
TopScrech <sergei_saliukov@icloud.com>
2024-11-27 12:51:05 +0100

    Removed SwiftData

---
+++ Everything SwiftUI/Everything SwiftUI.swift
@@ -0,0 +1,12 @@
+import ScrechKit
+
+@main
+struct EverythingSwiftUI: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HomeView()
+            }
+        }
+    }
+}
