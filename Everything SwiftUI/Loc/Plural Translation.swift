5fd871ae2953430bf94664a439def03c11efaa96
TopScrech <sergei_saliukov@icloud.com>
2025-04-01 13:44:00 +0200

    loc + loc section

---
+++ Everything SwiftUI/Loc/Plural Translation.swift
@@ -0,0 +1,25 @@
+import ScrechKit
+
+struct PluralTranslation: View {
+    private let days = [1, 2, 5, 1.5]
+    
+    var body: some View {
+        List {
+            ForEach(days, id: \.self) { days in
+                Text("\(days) days ago")
+            }
+            
+            Section {
+                Button {
+                    openSettings()
+                } label: {
+                    Label("Open Settings", systemImage: "gear")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PluralTranslation()
+}
