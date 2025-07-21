6b4ee6297197a5dba090897c281df7b045d62786
TopScrech <sergei_saliukov@icloud.com>
2025-07-21 09:01:33 +0200

    clearly highlighting unavailable topics

--- iOS/Nav/TopicLinkLabel.swift
+++ iOS/Nav/TopicLinkLabel.swift
@@ -7,17 +7,26 @@ struct TopicLinkLabel: View {
         self.topic = topic
     }
     
+    private var isAvailable: Bool {
+        topicView(topic) != nil
+    }
+    
     var body: some View {
         HStack {
             Label {
                 Text(topic.name)
+                
+                if !isAvailable {
+                    Text("Coming soon...")
+                }
             } icon: {
                 Image(systemName: topic.icon)
+                    .opacity(isAvailable ? 1 : 0.25)
                     .rotationEffect(.degrees(topic == .lazyVGrid ? 90 : 0))
             }
             .labelReservedIconWidth(16)
             
-            if topicView(topic) == nil {
+            if !isAvailable {
                 Spacer()
                 
                 Image(systemName: "lock")
