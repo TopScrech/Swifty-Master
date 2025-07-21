3d8fb5a4bf9fa6492f0acf0570f1df7c4060ae46
TopScrech <sergei_saliukov@icloud.com>
2025-07-21 14:32:00 +0200

    TopicAlert

--- iOS/Topics/View/TopicAlert.swift
+++ iOS/Topics/View/TopicAlert.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct TopicAlert: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("TopicAlert")
+            Text("In SwiftUI, you can present an alert by using the .alert modifier, which displays an alert with a title, message, and optional actions when the button is tapped")
             
             SimpleCodeBlockView(.alert)
         }
