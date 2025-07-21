1baf521f8ed06989452b30971e03a22728a98207
TopScrech <sergei_saliukov@icloud.com>
2025-07-21 14:30:21 +0200

    dark mode topic

--- iOS/Topics/System/TopicDarkMode.swift
+++ iOS/Topics/System/TopicDarkMode.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct TopicDarkMode: View {
     var body: some View {
         VStack(spacing: 25) {
+            Text("The ContentView struct uses the @Environment(\\.colorScheme) property wrapper to access the current color scheme environment variable, which determines whether the app is in Dark or Light mode")
+            
             SimpleCodeBlockView(.darkMode)
         }
     }
