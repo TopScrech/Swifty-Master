767c44925356da08e3da41ab044e7aba51002a11
TopScrech <sergei_saliukov@icloud.com>
2025-07-21 12:42:43 +0200

    related topics

--- iOS/Models/Topic.swift
+++ iOS/Models/Topic.swift
@@ -116,6 +116,25 @@ enum Topic: String, Identifiable, CaseIterable, Codable {
         case .image: [Topic.sfSymbols.id]
         case .sfSymbols: [Topic.image.id]
             
+        case .contextMenu: [Topic.menu.id]
+        case .menu: [Topic.contextMenu.id]
+            
+        case .vStack: [Topic.hStack.id]
+        case .hStack: [Topic.vStack.id]
+            
+        case .color: [Topic.gradient.id]
+        case .gradient: [Topic.color.id]
+            
+        case .grid: [Topic.lazyVGrid.id, Topic.lazyHGrid.id]
+        case .lazyHGrid: [Topic.grid.id, Topic.lazyVGrid.id]
+        case .lazyVGrid: [Topic.grid.id, Topic.lazyHGrid.id]
+            
+        case .scrollView: [Topic.list.id]
+        case .list: [Topic.scrollView.id]
+            
+        case .slider: [Topic.stepper.id]
+        case .stepper: [Topic.slider.id]
+            
         default: []
         }
     }
