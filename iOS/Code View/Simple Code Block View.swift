a570616c9413e80bddf809a1721e70aa0199b0bb
TopScrech <sergei_saliukov@icloud.com>
2025-07-30 21:21:19 +0300

    placed the copy button on the code block (instead of overlaying it)

--- iOS/Code View/Simple Code Block View.swift
+++ iOS/Code View/Simple Code Block View.swift
@@ -15,25 +15,27 @@ struct SimpleCodeBlockView: View {
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            Text(attributedCodeString(for: code))
-                .monospaced()
-                .padding(20)
-                .background(.ultraThinMaterial)
-                .cornerRadius(20)
-                .foregroundColor(.white)
-        }
-        .overlay(alignment: .bottomTrailing) {
-            Button {
-                UIPasteboard.general.string = code
-            } label: {
-                Image(systemName: "document.on.document")
-                    .title3(.semibold)
-                    .padding(10)
-                    .glassEffect(in: .rect(cornerRadius: 16))
-//                    .background(.ultraThinMaterial, in: .rect(cornerRadius: 8))
-                    .padding(8)
+            ZStack {
+                Text(attributedCodeString(for: code))
+                    .monospaced()
+                    .padding(20)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(20)
+                    .foregroundColor(.white)
+                
+                Button {
+                    UIPasteboard.general.string = code
+                } label: {
+                    Image(systemName: "document.on.document")
+                        .title3(.semibold)
+                        .padding(10)
+                        .glassEffect(in: .rect(cornerRadius: 16))
+                    //                    .background(.ultraThinMaterial, in: .rect(cornerRadius: 8))
+                        .padding(8)
+                }
+                .foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
             }
-            .foregroundStyle(.secondary)
         }
     }
 }
