96d737e864b599e851e25b6900984b262ffc8c76
TopScrech <sergei_saliukov@icloud.com>
2025-07-31 23:57:59 +0300

    improved reordering in the favorites

--- iOS/Nav/FavoritesList.swift
+++ iOS/Nav/FavoritesList.swift
@@ -3,12 +3,22 @@ import SwiftUI
 struct FavoritesList: View {
     @EnvironmentObject private var store: ValueStore
     
+    @State private var isEditing = false
+    
     var body: some View {
         List {
             if store.favoriteTopics.count > 0 {
                 ForEach(store.favoriteTopics) { topic in
                     NavigationLink(value: topic) {
-                        TopicLinkLabel(topic)
+                        HStack {
+                            TopicLinkLabel(topic)
+                            
+                            if isEditing {
+                                Image(systemName: "line.3.horizontal")
+                                    .title2()
+                                    .secondary()
+                            }
+                        }
                     }
                     .swipeActions(edge: .leading) {
                         if store.favoriteTopics.contains(topic) {
@@ -32,8 +42,23 @@ struct FavoritesList: View {
             }
         }
         .navigationTitle("Favorites")
+        .animation(.default, value: isEditing)
         .toolbar {
-            EditButton()
+            if isEditing {
+                Button {
+                    isEditing = false
+                } label: {
+                    Image(systemName: "checkmark")
+                }
+                .buttonStyle(.glassProminent)
+                .tint(.blue)
+            } else {
+                Button {
+                    isEditing = true
+                } label: {
+                    Image(systemName: "pencil")
+                }
+            }
         }
     }
     
