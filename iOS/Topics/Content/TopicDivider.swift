655e54ab106909c346fff24acf7d09c8a1ca0f27
TopScrech <sergei_saliukov@icloud.com>
2025-08-02 12:29:53 +0300

    -

--- iOS/Topics/Content/TopicDivider.swift
+++ iOS/Topics/Content/TopicDivider.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct TopicDivider: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("TopicDivider")
+            Text("Divider() in SwiftUI adds a visual line to separate content, helping structure your UI clearly. It’s especially useful in stacks and lists to improve readability and visual grouping")
+            
+            Divider()
             
             CodeBlockView(.divider)
         }
