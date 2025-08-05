c07e25f2414ee3b6a5c97b111b8338bf4fdbcfce
TopScrech <sergei_saliukov@icloud.com>
2025-08-05 14:41:55 +0300

    -

--- iOS/Models/Topic.swift
+++ iOS/Models/Topic.swift
@@ -91,32 +91,4 @@ enum Topic: String, Identifiable, CaseIterable, Codable {
             nil
         }
     }
-    
-    var category: Category {
-        switch self {
-        case .text, .textField, .textEditor, .image, .sfSymbols, .label, .shape, .divider, .customKeyboard:
-                .content
-            
-        case .button, .menu, .contextMenu, .toggle, .slider, .stepper, .picker, .gauges, .progressView, .badges:
-                .control
-            
-        case .sheet, .popover, .alert, .emptyView, .appStoreOverlay:
-                .view
-            
-        case .dismiss, .navigationView, .navigationStack, .navigationSplitView, .navigationBar, .navigationLink, .passAView, .link:
-                .navigation
-            
-        case .vStack, .hStack, .zStack, .list, .scrollView, .grid, .lazyVGrid, .lazyHGrid, .tabView:
-                .collections
-            
-        case .spacer, .padding, .frame, /*.positionPoint, */.geometryReader, .equalSizeViews:
-                .layout
-            
-        case .color, .gradient:
-                .design
-            
-        case .detectOSVersion, .detectDarkMode, .detectDeviceUsed, .differentiateOS, .detectScreenSize, .detectCompactOrRegular, .detectLang, .lowPowerMode:
-                .system
-        }
-    }
 }
---
+++ iOS/Models/TopiicCategory.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Topic {
+    var category: Category {
+        switch self {
+        case .text, .textField, .textEditor, .image, .sfSymbols, .label, .shape, .divider, .customKeyboard:
+                .content
+            
+        case .button, .menu, .contextMenu, .toggle, .slider, .stepper, .picker, .gauges, .progressView, .badges:
+                .control
+            
+        case .sheet, .popover, .alert, .emptyView, .appStoreOverlay:
+                .view
+            
+        case .dismiss, .navigationView, .navigationStack, .navigationSplitView, .navigationBar, .navigationLink, .passAView, .link:
+                .navigation
+            
+        case .vStack, .hStack, .zStack, .list, .scrollView, .grid, .lazyVGrid, .lazyHGrid, .tabView:
+                .collections
+            
+        case .spacer, .padding, .frame, /*.positionPoint, */.geometryReader, .equalSizeViews:
+                .layout
+            
+        case .color, .gradient:
+                .design
+            
+        case .detectOSVersion, .detectDarkMode, .detectDeviceUsed, .differentiateOS, .detectScreenSize, .detectCompactOrRegular, .detectLang, .lowPowerMode:
+                .system
+        }
+    }
+}
