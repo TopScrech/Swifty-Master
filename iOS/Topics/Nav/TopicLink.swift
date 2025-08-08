3cb29fd3a8a7018ee44ee7b47dc2ddad99ae73f5
TopScrech <sergei_saliukov@icloud.com>
2025-08-08 18:15:00 +0300

    improved link topic

--- iOS/Topics/Nav/TopicLink.swift
+++ iOS/Topics/Nav/TopicLink.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct TopicLink: View {
     var body: some View {
         VStack(spacing: 25) {
+            Text("Link")
+            
+#warning("This code block doesn't work")
             CodeBlockView(.link)
         }
     }
