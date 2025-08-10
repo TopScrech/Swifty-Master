b6ca7226fcc56df3f38b88d8a06d30999b4e94f7
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 13:17:07 +0300

    TopicLabel

--- iOS/Topics/Content/TopicLabel.swift
+++ iOS/Topics/Content/TopicLabel.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct TopicLabel: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("TopicLabel")
+            Text("The Label view combines text with an icon, which can come from SF Symbols, your asset catalog, or a fully custom view, and can be styled with different label styles")
             
             CodeBlockView(.label)
         }
