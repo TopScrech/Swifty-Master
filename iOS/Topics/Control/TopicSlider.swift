6086bf6601e3f7e87044accc6838cbd06416665f
TopScrech <sergei_saliukov@icloud.com>
2025-08-11 01:36:10 +0300

    -

--- iOS/Topics/Control/TopicSlider.swift
+++ iOS/Topics/Control/TopicSlider.swift
@@ -3,12 +3,10 @@ import SwiftUI
 struct TopicSlider: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("Slider is a customizable UI control that allows users to select a value from a continuous range")
-            
+            Text("Slider is a customizable UI control that allows users to select a value from a continuous range")      
 #if !os(visionOS)
             BrightnessSlider()
 #endif
-            
             CodeBlockView(.slider)
         }
     }
