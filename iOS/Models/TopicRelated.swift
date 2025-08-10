c6cbfeecfe7ae261d3fc726fead90960b9b0d346
TopScrech <sergei_saliukov@icloud.com>
2025-08-11 01:35:45 +0300

    -

--- iOS/Models/TopicRelated.swift
+++ iOS/Models/TopicRelated.swift
@@ -3,32 +3,32 @@ import Foundation
 extension Topic {
     var related: [Topic] {
         switch self {
-        case .textField: [.textEditor]
-        case .textEditor: [.textField]
+        case .textField:   [.textEditor]
+        case .textEditor:  [.textField]
             
-        case .image: [.sfSymbols]
-        case .sfSymbols: [.image]
+        case .image:       [.sfSymbols]
+        case .sfSymbols:   [.image]
             
         case .contextMenu: [.menu]
-        case .menu: [.contextMenu]
+        case .menu:        [.contextMenu]
             
-        case .vStack: [.hStack]
-        case .hStack: [.vStack]
+        case .vStack:      [.hStack]
+        case .hStack:      [.vStack]
             
-        case .color: [.gradient]
-        case .gradient: [.color]
+        case .color:       [.gradient]
+        case .gradient:    [.color]
             
-        case .grid: [.lazyGrids]
-        case .lazyGrids: [.grid]
+        case .grid:        [.lazyGrids]
+        case .lazyGrids:   [.grid]
             
-        case .scrollView: [.list]
-        case .list: [.scrollView]
+        case .scrollView:  [.list]
+        case .list:        [.scrollView]
             
-        case .slider: [.stepper]
-        case .stepper: [.slider]
+        case .slider:      [.stepper]
+        case .stepper:     [.slider]
             
-        case .spacer: [.divider]
-        case .divider: [.spacer]
+        case .spacer:      [.divider]
+        case .divider:     [.spacer]
             
         default: []
         }
