b22387e0cc131b09ca50f9e55042ab1947022b2b
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 13:41:30 +0300

    TopicMenu & badges

--- iOS/Topics/Control/TopicMenu.swift
+++ iOS/Topics/Control/TopicMenu.swift
@@ -5,6 +5,12 @@ struct TopicMenu: View {
         VStack(spacing: 25) {
             Text("Menu provides a customizable, collapsible list of options for user interaction")
             
+            Menu {
+                Button("Button") {}
+            } label: {
+                TopicActionButton("Menu", color: .blue.opacity(0.3))
+            }
+            
             CodeBlockView(.menu)
         }
     }
