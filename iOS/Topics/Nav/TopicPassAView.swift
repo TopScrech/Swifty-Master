fa2e4a53ac65ae489db522791b7f03538b3a1ccc
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 13:01:43 +0300

    TopicPassAView

--- iOS/Topics/Nav/TopicPassAView.swift
+++ iOS/Topics/Nav/TopicPassAView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct TopicPassAView: View {
     var body: some View {
         VStack(spacing: 25) {
+            Text("SwiftUI lets you pass a view into another view by storing it as a property, often using AnyView to handle different view types. This makes it easy to reuse layouts while swapping the displayed content")
+            
             CodeBlockView(.passAView)
         }
     }
