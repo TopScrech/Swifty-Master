161383f34fa24b5e83ec6471db73bafc01bf2fa0
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 14:45:34 +0300

    alert topic

--- iOS/Topics/View/TopicAlert.swift
+++ iOS/Topics/View/TopicAlert.swift
@@ -1,11 +1,23 @@
 import SwiftUI
+import MapKit
 
 struct TopicAlert: View {
+    @State private var alertExample = false
+    
     var body: some View {
         VStack(spacing: 25) {
             Text("In SwiftUI, you can present an alert by using the .alert modifier, which displays an alert with a title, message, and optional actions when the button is tapped")
             
+            TopicActionButton("Present an alert", color: .blue.opacity(0.3)) {
+                alertExample = true
+            }
+            
             CodeBlockView(.alert)
+        }
+        .alert("Title", isPresented: $alertExample) {
+            Button("Destructive buttton", role: .destructive) {}
+        } message: {
+            Text("Message")
         }
     }
 }
