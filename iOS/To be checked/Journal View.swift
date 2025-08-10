7f176c2978322cecc3ab263cdf52c99b24b64fb2
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 15:27:40 +0300

    some changes to the unused views

--- iOS/To be checked/Journal View.swift
+++ iOS/To be checked/Journal View.swift
@@ -1,36 +1,36 @@
 import SwiftUI
+import JournalingSuggestions
 
 struct JournalView: View {
-//    @State var suggestionPhotos = [JournalingSuggestion.Photo]()
-//    @State var suggestionTitle: String? = nil
+    @State var suggestionPhotos = [JournalingSuggestion.Photo]()
+    @State var suggestionTitle: String? = nil
     
     var body: some View {
         VStack {
-            //            Spacer().frame(height: 25)
-            //
-            //            SuggestionsPicker {
-            //                Text(buttonTitle)
-            //            } onCompletion: { suggestion in
-            //                // Parse the suggestion details
-            //                suggestionTitle = suggestion.title
-            //                suggestionPhotos = await suggestion.content(forType: JournalingSuggestion.Photo.self)
-            //            }
-            //
-            //            // Display the suggestion details
-            //            Spacer().frame(height: 25)
-            //
-            //            Text(suggestionTitle ?? "")
-            //
-            //            List {
-            //                ForEach(suggestionPhotos, id: \.photo) { item in
-            //                    AsyncImage(url: item.photo) { image in
-            //                        image.image?
-            //                            .resizable ()
-            //                            .aspectRatio(contentMode: .fit)
-            //                    }
-            //                    .frame(maxHeight: 200)
-            //                }
-            //            }
+            JournalingSuggestionsPicker {
+                Text("Title")
+            } onCompletion: { suggestion in
+                // Parse the suggestion details
+                suggestionTitle = suggestion.title
+                suggestionPhotos = await suggestion.content(forType: JournalingSuggestion.Photo.self)
+            }
+            
+            // Display the suggestion details
+            Spacer()
+                .frame(height: 25)
+            
+            Text(suggestionTitle ?? "")
+            
+            List {
+                ForEach(suggestionPhotos, id: \.photo) { item in
+                    AsyncImage(url: item.photo) { image in
+                        image.image?
+                            .resizable ()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .frame(maxHeight: 200)
+                }
+            }
         }
     }
 }
