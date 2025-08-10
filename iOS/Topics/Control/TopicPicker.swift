2e8dec666f052b17266a911428d7c77588b9b58f
TopScrech <sergei_saliukov@icloud.com>
2025-08-10 15:04:35 +0300

    interactive picker topic

--- iOS/Topics/Control/TopicPicker.swift
+++ iOS/Topics/Control/TopicPicker.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct TopicPicker: View {
+    @State private var favoriteAnimal = "Dog"
+    
     var body: some View {
         VStack(spacing: 25) {
             Text("SwiftUI's Picker is a customizable, interactive way allowing users to select from a list of options")
+            
+            Picker("Favorite animal", selection: $favoriteAnimal) {
+                Text("Dog")
+                    .tag("Dog")
+                
+                Text("Cat")
+                    .tag("Cat")
+                
+                Text("Fish")
+                    .tag("Fish")
+            }
             
             CodeBlockView(.picker)
         }
