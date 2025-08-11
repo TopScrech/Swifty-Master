ad0cb4fa9fef4a3b02d3f7d68b06ecc75fc70826
TopScrech <sergei_saliukov@icloud.com>
2025-08-12 01:11:29 +0300

    tvOS: created its target

---
+++ Swifty Master/Swifty_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_MasterApp.swift
+//  Swifty Master
+//
+//  Created by Sergei Saliukov on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
