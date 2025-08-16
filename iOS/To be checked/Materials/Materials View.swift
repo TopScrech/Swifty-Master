b486473ca76f7b8c0481d9d29ca893806521f875
TopScrech <sergei_saliukov@icloud.com>
2025-08-17 00:40:43 +0200

    materials displayed in a grid with 2 columns

--- iOS/To be checked/Materials/Materials View.swift
+++ iOS/To be checked/Materials/Materials View.swift
@@ -2,17 +2,22 @@ import SwiftUI
 
 struct MaterialsView: View {
     private let materials = [
-        ("None", nil),
-        ("Ultra Thin", .ultraThin),
+        ("Ultra Thin", Material.ultraThin),
         ("Thin", .thin),
         ("Regular", .regular),
         ("Thick", .thick),
         ("Ultra Thick", .ultraThick),
-        ("Bar", Material.bar)
+        ("Bar", .bar),
+        ("None", nil),
+    ]
+    
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
     ]
     
     var body: some View {
-        VStack {
+        LazyVGrid(columns: columns, spacing: 32) {
             ForEach(0..<materials.count, id: \.self) { index in
                 let (key, value) = materials[index]
                 
@@ -35,7 +40,7 @@ struct MaterialsView: View {
                         }
                     }
                 }
-                .frame(300)
+                .frame(160)
             }
         }
     }
