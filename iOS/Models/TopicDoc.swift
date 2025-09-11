a1c9d57033ada70fbcb091d5b3c50eb2586b4206
TopScrech <sergei_saliukov@icloud.com>
2025-09-11 10:32:59 +0200

    fixed a warning

--- iOS/Models/TopicDoc.swift
+++ iOS/Models/TopicDoc.swift
@@ -14,22 +14,22 @@ struct TopicDocType: Identifiable {
     }
 }
 
-#warning("Remove all returns after Xcode 26 is out")
 extension Topic {
     var docs: [TopicDocType] {
         switch self {
             // MARK: - Content
         case .text:
-            return [
+            [
                 TopicDocType("Text", url: "https://developer.apple.com/documentation/swiftui/text"),
                 TopicDocType("font(_:)", url: "https://developer.apple.com/documentation/swiftui/text/font(_:)"),
                 TopicDocType("fontDesign(_:)", url: "https://developer.apple.com/documentation/swiftui/text/fontdesign(_:)"),
                 TopicDocType("fontWeight(_:)", url: "https://developer.apple.com/documentation/swiftui/text/fontweight(_:)"),
-                TopicDocType("fontWidth(_:)", url: "https://developer.apple.com/documentation/swiftui/text/fontwidth(_:)")
+                TopicDocType("fontWidth(_:)", url: "https://developer.apple.com/documentation/swiftui/text/fontwidth(_:)"
+                            )
             ]
             
         case .textField:
-            return [
+            [
                 TopicDocType("Text Field", url: "https://developer.apple.com/documentation/swiftui/textfield"),
                 TopicDocType("textFieldStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/textfieldstyle(_:)"),
                 TopicDocType("keyboardType(_:)", url: "https://developer.apple.com/documentation/swiftui/view/keyboardtype(_:)"),
@@ -37,13 +37,13 @@ extension Topic {
             ]
             
         case .textEditor:
-            return [
+            [
                 TopicDocType("Text Editor", url: "https://developer.apple.com/documentation/swiftui/texteditor"),
                 TopicDocType("init(text:selection:) (iOS 26)", url: "https://developer.apple.com/documentation/swiftui/texteditor/init(text:selection:)")
             ]
             
         case .shape:
-            return [
+            [
                 TopicDocType("Rectangle", url: "https://developer.apple.com/documentation/swiftui/rectangle"),
                 TopicDocType("RoundedRectangle", url: "https://developer.apple.com/documentation/swiftui/roundedrectangle"),
                 TopicDocType("Circle", url: "https://developer.apple.com/documentation/swiftui/circle"),
@@ -54,30 +54,32 @@ extension Topic {
             
             // MARK: - Image & Symbols
         case .image:
-            return [
+            [
                 TopicDocType("Image", url: "https://developer.apple.com/documentation/swiftui/image"),
                 TopicDocType("Image(systemName:)", url: "https://developer.apple.com/documentation/swiftui/image/init(systemname:)"),
                 TopicDocType("SF Symbols macOS app", url: "https://developer.apple.com/sf-symbols")
             ]
             
         case .divider:
-            return [TopicDocType("Divider", url: "https://developer.apple.com/documentation/swiftui/divider")]
+            [TopicDocType("Divider", url: "https://developer.apple.com/documentation/swiftui/divider")]
             
             // MARK: - Control
         case .gauges:
-            return [
+            [
                 TopicDocType("Gauge", url: "https://developer.apple.com/documentation/swiftui/gauge"),
-                TopicDocType("gaugeStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/gaugestyle(_:)")
+                TopicDocType("gaugeStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/gaugestyle(_:)"
+                            )
             ]
             
         case .progressView:
-            return [
+            [
                 TopicDocType("ProgressView", url: "https://developer.apple.com/documentation/swiftui/progressview"),
-                TopicDocType("progressViewStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/progressviewstyle(_:)")
+                TopicDocType("progressViewStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/progressviewstyle(_:)"
+                            )
             ]
             
         case .button:
-            return [
+            [
                 TopicDocType("Button", url: "https://developer.apple.com/documentation/swiftui/button"),
                 TopicDocType("buttonStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/buttonstyle(_:)"),
                 TopicDocType("buttonBorderShape(_:)", url: "https://developer.apple.com/documentation/swiftui/view/buttonbordershape(_:)"),
@@ -85,127 +87,132 @@ extension Topic {
             ]
             
         case .toggle:
-            return [
+            [
                 TopicDocType("Toggle", url: "https://developer.apple.com/documentation/swiftui/toggle"),
                 TopicDocType("Toggles", url: "https://developer.apple.com/design/human-interface-guidelines/toggles")
             ]
             
         case .menu:
-            return [
+            [
                 TopicDocType("Menu", url: "https://developer.apple.com/documentation/swiftui/menu"),
-                TopicDocType("menuStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/menustyle(_:)")
+                TopicDocType("menuStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/menustyle(_:)"
+                            )
             ]
             
         case .slider:
-            return [TopicDocType("Slider", url: "https://developer.apple.com/documentation/swiftui/slider")]
+            [TopicDocType("Slider", url: "https://developer.apple.com/documentation/swiftui/slider")]
             
         case .stepper:
-            return [TopicDocType("Stepper", url: "https://developer.apple.com/documentation/swiftui/stepper")]
+            [TopicDocType("Stepper", url: "https://developer.apple.com/documentation/swiftui/stepper")]
             
         case .picker:
-            return [
+            [
                 TopicDocType("Picker", url: "https://developer.apple.com/documentation/swiftui/picker"),
-                TopicDocType("pickerStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/pickerstyle(_:)")
+                TopicDocType("pickerStyle(_:)", url: "https://developer.apple.com/documentation/swiftui/view/pickerstyle(_:)"
+                            )
             ]
             
         case .contextMenu:
-            return [TopicDocType("ContextMenu", url: "https://developer.apple.com/documentation/swiftui/view/contextmenu(menuitems:)")]
+            [TopicDocType("ContextMenu", url: "https://developer.apple.com/documentation/swiftui/view/contextmenu(menuitems:)")]
             
             // MARK: - View
         case .sheet:
-            return [TopicDocType("sheet(item:onDismiss:content:)", url: "https://developer.apple.com/documentation/swiftui/view/sheet(item:onDismiss:content:)")]
+            [TopicDocType("sheet(item:onDismiss:content:)", url: "https://developer.apple.com/documentation/swiftui/view/sheet(item:onDismiss:content:)")]
             
         case .popover:
-            return [TopicDocType("popover(isPresented:attachmentAnchor:arrowEdge:content:)", url: "https://developer.apple.com/documentation/swiftui/view/popover(ispresented:attachmentanchor:arrowedge:content:)")]
+            [TopicDocType("popover(isPresented:attachmentAnchor:arrowEdge:content:)", url: "https://developer.apple.com/documentation/swiftui/view/popover(ispresented:attachmentanchor:arrowedge:content:)")]
             
         case .alert:
-            return [TopicDocType("alert(_:ispresented:actions:message:)", url: "https://developer.apple.com/documentation/swiftui/view/alert(_:ispresented:actions:message:)-6awwp")]
+            [TopicDocType("alert(_:ispresented:actions:message:)", url: "https://developer.apple.com/documentation/swiftui/view/alert(_:ispresented:actions:message:)-6awwp")]
             
         case .emptyView:
-            return [TopicDocType("EmptyView", url: "https://developer.apple.com/documentation/swiftui/emptyview")]
+            [TopicDocType("EmptyView", url: "https://developer.apple.com/documentation/swiftui/emptyview")]
             
         case .appStoreOverlay:
-            return [
+            [
                 TopicDocType("appStoreOverlay(ispresented:configuration:)", url: "https://developer.apple.com/documentation/swiftui/view/appstoreoverlay(ispresented:configuration:)"),
                 TopicDocType("SKOverlay", url: "https://developer.apple.com/documentation/storekit/skoverlay")
             ]
             
         case .badges:
-            return [
+            [
                 TopicDocType("applicationIconBadgeNumber (Deprecated, iOS 2.0 - 16.0)", url: "https://developer.apple.com/documentation/uikit/uiapplication/applicationiconbadgenumber"),
-                TopicDocType("setBadgeCount(_:withCompletionHandler:)", url: "https://developer.apple.com/documentation/UserNotifications/UNUserNotificationCenter/setBadgeCount(_:withCompletionHandler:)")
+                TopicDocType("setBadgeCount(_:withCompletionHandler:)", url: "https://developer.apple.com/documentation/UserNotifications/UNUserNotificationCenter/setBadgeCount(_:withCompletionHandler:)"
+                            )
             ]
             
             // MARK: - Navigation
         case .dismiss:
-            return [TopicDocType("dismiss()", url: "https://developer.apple.com/documentation/swiftui/environmentvalues/dismiss")]
+            [TopicDocType("dismiss()", url: "https://developer.apple.com/documentation/swiftui/environmentvalues/dismiss")]
             
         case .navigationView:
-            return [TopicDocType("NavigationView (Deprecated)", url: "https://developer.apple.com/documentation/swiftui/navigationview")]
+            [TopicDocType("NavigationView (Deprecated)", url: "https://developer.apple.com/documentation/swiftui/navigationview")]
             
         case .navigationStack:
-            return [TopicDocType("NavigationStack", url: "https://developer.apple.com/documentation/swiftui/navigationstack")]
+            [TopicDocType("NavigationStack", url: "https://developer.apple.com/documentation/swiftui/navigationstack")]
             
         case .navigationSplitView:
-            return [TopicDocType("NavigationSplitView", url: "https://developer.apple.com/documentation/swiftui/navigationsplitview")]
+            [TopicDocType("NavigationSplitView", url: "https://developer.apple.com/documentation/swiftui/navigationsplitview")]
             
         case .navigationLink:
-            return [TopicDocType("NavigationLink", url: "https://developer.apple.com/documentation/swiftui/navigationlink")]
+            [TopicDocType("NavigationLink", url: "https://developer.apple.com/documentation/swiftui/navigationlink")]
             
         case .navigationBar:
-            return [TopicDocType("toolbar(content:)", url: "https://developer.apple.com/documentation/swiftui/view/toolbar(content:)")]
+            [TopicDocType("toolbar(content:)", url: "https://developer.apple.com/documentation/swiftui/view/toolbar(content:)")]
             
         case .passAView:
-            return [TopicDocType("AnyView", url: "https://developer.apple.com/documentation/swiftui/anyview")]
+            [TopicDocType("AnyView", url: "https://developer.apple.com/documentation/swiftui/anyview")]
             
         case .link:
-            return [TopicDocType("Link", url: "https://developer.apple.com/documentation/swiftui/link")]
+            [TopicDocType("Link", url: "https://developer.apple.com/documentation/swiftui/link")]
             
             // MARK: - Collections
         case .vStack:
-            return [TopicDocType("VStack", url: "https://developer.apple.com/documentation/swiftui/vstack")]
+            [TopicDocType("VStack", url: "https://developer.apple.com/documentation/swiftui/vstack")]
             
         case .hStack:
-            return [TopicDocType("HStack", url: "https://developer.apple.com/documentation/swiftui/hstack")]
+            [TopicDocType("HStack", url: "https://developer.apple.com/documentation/swiftui/hstack")]
             
         case .zStack:
-            return [TopicDocType("ZStack", url: "https://developer.apple.com/documentation/swiftui/zstack")]
+            [TopicDocType("ZStack", url: "https://developer.apple.com/documentation/swiftui/zstack")]
             
         case .grids:
-            return [
+            [
                 TopicDocType("Grid", url: "https://developer.apple.com/documentation/swiftui/grid"),
                 TopicDocType("LazyVGrid", url: "https://developer.apple.com/documentation/swiftui/lazyvgrid"),
                 TopicDocType("LazyHGrid", url: "https://developer.apple.com/documentation/swiftui/lazyhgrid")
             ]
             
         case .list:
-            return [TopicDocType("List", url: "https://developer.apple.com/documentation/swiftui/list")]
+            [TopicDocType("List", url: "https://developer.apple.com/documentation/swiftui/list")]
             
         case .scrollView:
-            return [TopicDocType("ScrollView", url: "https://developer.apple.com/documentation/swiftui/scrollview")]
+            [TopicDocType("ScrollView", url: "https://developer.apple.com/documentation/swiftui/scrollview")]
             
         case .tabView:
-            return [TopicDocType("TabView", url: "https://developer.apple.com/documentation/swiftui/tabview")]
+            [TopicDocType("TabView", url: "https://developer.apple.com/documentation/swiftui/tabview")]
             
             // MARK: - Layout
         case .padding:
-            return [TopicDocType("padding(_:_:)", url: "https://developer.apple.com/documentation/swiftui/view/padding(_:_:)")]
+            [TopicDocType("padding(_:_:)", url: "https://developer.apple.com/documentation/swiftui/view/padding(_:_:)"
+                         )]
             
         case .frame:
-            return [TopicDocType("frame(width:height:alignment:)", url: "https://developer.apple.com/documentation/swiftui/view/frame(width:height:alignment:)")]
+            [TopicDocType("frame(width:height:alignment:)", url: "https://developer.apple.com/documentation/swiftui/view/frame(width:height:alignment:)"
+                         )]
             
         case .geometryReader:
-            return [TopicDocType("GeometryReader", url: "https://developer.apple.com/documentation/swiftui/geometryreader")]
+            [TopicDocType("GeometryReader", url: "https://developer.apple.com/documentation/swiftui/geometryreader")]
             
         case .spacer:
-            return [TopicDocType("Spacer", url: "https://developer.apple.com/documentation/swiftui/spacer")]
+            [TopicDocType("Spacer", url: "https://developer.apple.com/documentation/swiftui/spacer")]
             
             // MARK: - Color
         case .color:
-            return [TopicDocType("Color", url: "https://developer.apple.com/documentation/swiftui/color")]
+            [TopicDocType("Color", url: "https://developer.apple.com/documentation/swiftui/color")]
             
         case .gradient:
-            return [
+            [
                 TopicDocType("Gradient", url: "https://developer.apple.com/documentation/swiftui/gradient"),
                 TopicDocType("LinearGradient", url: "https://developer.apple.com/documentation/swiftui/lineargradient"),
                 TopicDocType("RadialGradient", url: "https://developer.apple.com/documentation/swiftui/radialgradient"),
@@ -215,21 +222,22 @@ extension Topic {
             
             // MARK: - System
         case .detectDarkMode:
-            return [TopicDocType("colorScheme", url: "https://developer.apple.com/documentation/swiftui/environmentvalues/colorscheme")]
+            [TopicDocType("colorScheme", url: "https://developer.apple.com/documentation/swiftui/environmentvalues/colorscheme")]
             
-        case .detectOSVersion: return [
-            TopicDocType("UIUserInterfaceIdiom", url: "https://developer.apple.com/documentation/uikit/uiuserinterfaceidiom"),
-            TopicDocType("Running code on a specific platform or OS version", url: "https://developer.apple.com/documentation/xcode/running-code-on-a-specific-version")
-        ]
+        case .detectOSVersion:
+            [
+                TopicDocType("UIUserInterfaceIdiom", url: "https://developer.apple.com/documentation/uikit/uiuserinterfaceidiom"),
+                TopicDocType("Running code on a specific platform or OS version", url: "https://developer.apple.com/documentation/xcode/running-code-on-a-specific-version")
+            ]
             
         case .detectLang:
-            return [TopicDocType("preferredLocalizations", url: "https://developer.apple.com/documentation/foundation/bundle/preferredlocalizations")]
+            [TopicDocType("preferredLocalizations", url: "https://developer.apple.com/documentation/foundation/bundle/preferredlocalizations")]
             
         case .lowPowerMode:
-            return [TopicDocType("isLowPowerModeEnabled", url: "https://developer.apple.com/documentation/foundation/processinfo/islowpowermodeenabled")]
+            [TopicDocType("isLowPowerModeEnabled", url: "https://developer.apple.com/documentation/foundation/processinfo/islowpowermodeenabled")]
             
         default:
-            return []
+            []
         }
     }
 }
