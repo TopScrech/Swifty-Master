761e06baad97e18d32bab70dd26a4df81caea02c
TopScrech <sergei_saliukov@icloud.com>
2026-03-19 10:43:32 +0100

    moved glassyBackground to ScrechKit

--- iOS/Utils/iOS 18 support.swift
+++ iOS/Utils/iOS 18 support.swift
@@ -2,39 +2,6 @@ import SwiftUI
 
 extension View {
     @ViewBuilder
-    func glassyBackground(_ rectRounding: CGFloat? = nil) -> some View {
-#if os(visionOS)
-        if let rectRounding {
-            self
-                .background(.ultraThinMaterial, in: .rect(cornerRadius: rectRounding))
-        } else {
-            self
-                .background(.ultraThinMaterial)
-        }
-#else
-        if #available(iOS 26, macOS 26, tvOS 26, *) {
-            if let rectRounding {
-                self
-                    .glassEffect(in: .rect(cornerRadius: rectRounding))
-            } else {
-                self
-                    .glassEffect()
-            }
-        } else {
-            if let rectRounding {
-                self
-                    .background(.ultraThinMaterial, in: .rect(cornerRadius: rectRounding))
-            } else {
-                self
-                    .background(.ultraThinMaterial)
-            }
-        }
-#endif
-    }
-}
-
-extension View {
-    @ViewBuilder
     func glassProminentButtonStyle(_ color: Color) -> some View {
         if #available(iOS 26, macOS 26, tvOS 26, *) {
             self
