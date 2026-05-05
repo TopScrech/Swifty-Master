edc858cb93954856407342cc05b06beb28a86e8a
TopScrech <sergei_saliukov@icloud.com>
2026-05-05 22:52:10 +0200

    Updated text field article

--- iOS/Topics/Content/TopicTextField.swift
+++ iOS/Topics/Content/TopicTextField.swift
@@ -14,7 +14,16 @@ struct TopicTextField: View {
             CodeBlockView(.textField)
             
             TopicDisclosureGroup("Secured TextField") {
+                Text("Use a secure field when you want the behavior of a TextField, but you want to hide the field’s text. Typically, you use this for entering passwords and other sensitive information")
+                
                 CodeBlockView(.textFieldSecured)
+                
+                Text("""
+- Displays one dot for each character someone types
+- Hides the dots when someone takes a screenshot or records the screen in iOS
+- Prevents anyone from cutting or copying the field’s contents
+- Displays an indicator when Caps Lock is enabled
+""")
             }
             
             TopicDisclosureGroup("Custom Keyboard") {
