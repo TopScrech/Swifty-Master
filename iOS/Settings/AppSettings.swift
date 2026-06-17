b0e16d3bfbdd7e4995fd985efe13c00c131bcfa5
TopScrech <sergei_saliukov@icloud.com>
2026-06-17 12:52:41 +0200

    Added github link

--- iOS/Settings/AppSettings.swift
+++ iOS/Settings/AppSettings.swift
@@ -31,6 +31,25 @@ struct AppSettings: View {
 #endif
                 NavModeButton()
             }
+            
+            Section {
+                let url = URL(string: "https://github.com/TopScrech/Swifty-Master")!
+                
+                Link(destination: url) {
+                    HStack(spacing: 12) {
+                        Image(.gitHub)
+                            .resizable()
+                            .frame(24)
+                            .clipShape(.circle)
+                        
+                        Text("GitHub")
+                    }
+                }
+                .tint(.primary)
+            } footer: {
+                Text("Bug reports, feature requests & contributions are always welcome!")
+                    .secondary()
+            }
         }
         .navigationTitle("Settings")
         .formStyle(.grouped)
